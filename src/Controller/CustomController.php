6bbb1f0e268ab76e4411c8bd4071897171063a40
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-10-21 16:08:16 -0300

    Create page customized with fancybox and doctrine

--- src/Controller/CustomController.php
+++ src/Controller/CustomController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 
 
+use App\Entity\Post;
 use Sonata\AdminBundle\Controller\CRUDController;
 
 class CustomController extends CRUDController
 {
     public function exibirAction()
     {
-        return $this->renderWithExtraParams('admin/custom/exibir.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository(Post::class)->findAll();
+        return $this->renderWithExtraParams('admin/custom/exibir.html.twig', compact('posts'));
     }
 }
