342ea77cd7845561ce71376d09eb280f962496d0
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-10-21 12:29:37 -0300

    Add filters in posts

--- src/Admin/PostAdmin.php
+++ src/Admin/PostAdmin.php
@@ -5,9 +5,11 @@ namespace App\Admin;
 use App\Entity\Category;
 use App\Entity\Post;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\ModelType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -42,6 +44,16 @@ class PostAdmin extends AbstractAdmin
             ->add('content', TextareaType::class)
             ->add('status', CheckboxType::class, [
                 'required' => false
+            ]);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter->add('title')
+            ->add('status')
+            ->add('category', null, [], EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
             ]);
     }
 
